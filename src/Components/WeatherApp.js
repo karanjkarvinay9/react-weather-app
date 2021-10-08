1718cb20764c5d7648bed5336f66df821e42ad37
vinay <karanjkarvinay9@gmail.com>
2021-10-08 16:04:00 +0530

    Weather App with updated UI and search functionality

--- src/Components/WeatherApp.js
+++ src/Components/WeatherApp.js
@@ -3,31 +3,34 @@ import WeatherUI from './WeatherUI';
 
 const WeatherApp = () => {
     const [weather, setWeather] = useState({});
-
-    const getweatherInfo = async ()=> {
+    let t = false;
+    const getweatherInfo = async (city)=> {
       try {
-       let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=nashik&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+        city = city ? city : "pune"
+       let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
        const data = await res.json();
-       const { main:weather } = data.weather[0];
+       const { main:weather,icon } = data.weather[0];
        const {temp,humidity} = data.main;
        const {speed} = data.wind;
        const {country} = data.sys;
        const {name,timezone} = data;
        const obj = {
-         temp,humidity,speed,country,name,weather,timezone
+         temp,humidity,speed,country,name,weather,timezone,icon
        }
        setWeather(obj);
       } catch (error) {
+        t=true;
         console.log(error);
+        alert("please Enter Valid City")
       }
     }
   
     useEffect(() => {
       getweatherInfo();
-    }, [])
+    },[])
     return (
         <div className="container vertical-center">
-        <WeatherUI obj = { weather }/>
+        <WeatherUI obj = { weather } getWeatherData={getweatherInfo} t = {t}/>
         </div>
     )
 }
--- src/Components/WeatherUI.js
+++ src/Components/WeatherUI.js
@@ -1,40 +1,65 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import moment from 'moment';
 import 'moment-timezone';
 
 
 
-const WeatherUI = ({ obj }) => {    
+const WeatherUI = ({ obj,getWeatherData}) => {
+    const [weatherIcon,setWeatherIcon] = useState("./sunny.png")
+    const [searchValue,setSearchValue] = useState("")
     const {
-        temp,humidity,speed,country,name,weather,timezone
+        temp,humidity,speed,country,name,weather,timezone,icon
       } = obj;
+
+   
 
       var dec = moment().utc().utcOffset(timezone/60);
       const mom =dec.format('LT');
 
+      const getDataAndSetValue = () =>{
+        getWeatherData(searchValue);
+        setSearchValue("");
+      }
+
+
+      useEffect(() => {
+          setWeatherIcon(`http://openweathermap.org/img/wn/${icon}.png`)
+      }, [icon])
+
     return (
-        <div className="container-fluid">
-        <div className="row justify-content-center">
+        <div className="container-fluid ">
+        <div className="row justify-content-center  ">
         <div className="col-12 col-md-4 col-sm-12 col-xs-12">
-        <div className="card p-4">
-        <div className="d-flex">
+        <div className="form-inline input-group">
+        <input className="form-control mr-sm-2 col-md-4" type="search"
+         placeholder="Search" aria-label="Search"
+         value = {searchValue} 
+         onChange={(event)=>{ setSearchValue(event.target.value) }}
+         onKeyPress = {(e) => {if(e.key ==="Enter") getDataAndSetValue() }}
+         />
+      <button className="btn btn-outline-success input-group-prepend col-md-4" 
+      type="submit" onClick={()=>{getDataAndSetValue() }} >Search</button>
+      </div>
+      <br/>
+        <div className="card p-4" style = {{"backgroundColor" : '#FFCB9A'}}>
+        <div className="d-flex ">
             <h6 className="flex-grow-1">{name},{country}</h6>
             <h6>{ mom }</h6>
         </div>
-        <div className="d-flex flex-column temp mt-5 mb-3">
+        <div className="d-flex flex-column temp mt-5 mb-3 ">
             <h1 className="mb-0 font-weight-bold" id="heading"> {temp}&deg; C </h1> 
             <span className="small grey">{weather}</span>
         </div>
         <div className="d-flex">
         <div className="temp-details flex-grow-1">
-            <p className="my-1"> <img src="https://i.imgur.com/B9kqOzp.png" height="17px"/> 
+            <p className="my-1"> <img src="https://i.imgur.com/B9kqOzp.png" height="17px" alt="wind-speed"/> 
             <span> {speed} km/h </span> </p>
             <p className="my-1"> <i className="fa fa-tint mr-2" aria-hidden="true"></i>
              <span> {humidity}% </span> </p>
-            <p className="my-1"> <img src="https://i.imgur.com/wGSJ8C5.png" height="17px"/>
+            <p className="my-1"> <img src="https://i.imgur.com/wGSJ8C5.png" height="17px" alt="sunrise" />
              <span> 0.2h </span> </p>
         </div>
-        <div > <i className= "wi  wi-cloud"/> </div>
+        <div > <img src={weatherIcon} width="100px" alt="weather-icon"/>  </div>
         </div>
         </div>
         </div>
